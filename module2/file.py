7ea42795544f1a5af71da27a770b9879008fa841
bungongtoto <bungongkingsley@gmail.com>
2024-12-04 11:50:59 +0000

    module 2

---
+++ module2/file.py
@@ -0,0 +1,22 @@
+#practice exo 1
+var_integer = 15
+print(type(var_integer))
+
+#practice exo 2
+var_float = 6.36272
+print(type(var_float))
+
+#practice exo 4
+var_boolean = True
+print(var_boolean)
+
+#practice exo 4
+
+first_name = "Kingsley"
+middle_name = "Ndzi"
+last_name = "Bungong"
+
+full_name = first_name + " " + middle_name + " " + last_name
+
+print(full_name)
+
