8eadd197362d4495eff8357aab7e6abc6cea7043
bungongtoto <bungongkingsley@gmail.com>
2024-12-04 12:17:29 +0000

    module 3

---
+++ module3/multiple_assignment_statements.py
@@ -0,0 +1,24 @@
+# a = b = c = 4
+# print(a)
+# print(b)
+# print(c)
+
+# numbers = {"First Num": "1", "Second": "2"}
+#
+# for key, value in numbers.items():
+#     print(f"The Key {key} has value {value}")
+
+#exo 1
+
+var_1, var_2, var_3 = 10, 20, 30
+
+#exo 2
+
+int_var, str1_var, float_var, str2_var = 33, "car", 2.158, "hey"
+
+#exo 3
+
+var_ages = {"Dave": "41", "Bob": "22", "Mark": "38"}
+
+for key, value in var_ages.items():
+    print(F"{key} is {value} years old")
