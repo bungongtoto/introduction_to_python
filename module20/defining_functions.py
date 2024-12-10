39c83f8101723795502f308da74dace13ae01fef
bungongtoto <bungongkingsley@gmail.com>
2024-12-10 09:34:50 +0000

    defining funcions

---
+++ module20/defining_functions.py
@@ -0,0 +1,12 @@
+import hello
+#Here is our first function
+def first_function():
+    print("Hello World")
+
+# first_function()
+print("==========================================================================")
+#second function
+def second_function(name):
+    print("Hello "+ name)
+
+second_function("Kingsley")
---
+++ module20/hello.py
@@ -0,0 +1,4 @@
+#creating our first module
+
+def greet():
+    print("Hello there, how are you?")
